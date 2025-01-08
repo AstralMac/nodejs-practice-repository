cd8ddb7502093cca3da7fdb0ee8927f543ac86be
Malcolm Abdullah <mabdullah4562@gmail.com>
2025-01-07 20:54:48 -0600

    Update

--- src/utils/Area-Circle.js
+++ src/utils/Area-Circle.js
@@ -9,14 +9,17 @@
 
 // This function calculates the area of a circle
 function calculateArea(radius){
+  //Enures that the input is a number and throws an error if it is not.
   if (typeof radius !== 'number'){
     throw new Error ('Radius must be a number');
   }
 
+  //Throws an error if the input is less than zero
   if (radius < 0){
     throw new Error("Radius can't be a negative number");
   }
-  
+
+  //Calculates the area of a circle by mutlplying the radius twice by PI
   return Math.PI * radius * radius;
   }
 
--- test/utils/Area-Circle.spec.js
+++ test/utils/Area-Circle.spec.js
@@ -7,22 +7,23 @@
  */
 
 
-const calculateArea = require('../../src/utils/Area-Circle'); // Import the factorial function from the factorial.js file
+const calculateArea = require('../../src/utils/Area-Circle'); // Import the calculateArea function from the Area-Circle.js file
 
-// The describe() function is a test suite that contains one or more tests
+// Suite of tests following the TDD principles. Testing a function that calculates the area of a circle
 describe('Area Function', () => {
+  //This test will ensure that the input is a number.
   it('Make sure the input is valid',()=>{
     const radius =5;
     const getArea = Math.PI * radius * radius;
     expect(calculateArea(radius)).toBeCloseTo(getArea);
 
   });
-
+//Throws error if input is a negative number.
   it('Throw an error when input is a negative number',()=>{
     const radius = -13848534580380503458;
     expect(()=> calculateArea(radius)).toThrow("Radius can't be a negative number");
   });
-
+//Ensures that if the radisus is zero taht the area is also zero.
   it('Radius is zero',()=>{
     const radius = 0;
     expect(calculateArea(radius)).toBe(0);
